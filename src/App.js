6406d6f906b0b1f8ee63d3ae8cf8ed2f17312add
goblin <factotumfrankie@gmail.com>
2022-10-07 17:15:42 -0700

    added header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './appComponents/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/appComponents/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+export default function Header() {
+
+    return (
+        <div id='header'>
+            <h1>goblin's palette generator</h1>
+        </div>
+    )
+
+};
