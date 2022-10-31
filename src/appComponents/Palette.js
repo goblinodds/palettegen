fac2f3a520acae3e2a5789b5e07324dd894a036e
goblin <factotumfrankie@gmail.com>
2022-10-30 18:39:33 -0700

    functioning (static) value palette

--- src/appComponents/Palette.js
+++ src/appComponents/Palette.js
@@ -3,11 +3,11 @@ import ReactSlider from 'react-slider';
 import { useState } from 'react';
 import { formatHex8 } from 'culori';
 
-// https://twitter.com/goblincodes/status/1586878836553453571?s=20&t=vhQoNDyIRnvPif-CzAXPgQ
-
 // https://culorijs.org/api/
 
-// 7 swatches to start with
+// STATIC VERSION
+// set up functions that do the other steps from this:
+// https://twitter.com/goblincodes/status/1586878836553453571?s=20&t=vhQoNDyIRnvPif-CzAXPgQ
 
 // LATER VERSIONS
 //// increase/decrease # of swatches: https://twitter.com/goblincodes/status/1586880288495263744?s=20&t=vhQoNDyIRnvPif-CzAXPgQ
@@ -17,59 +17,58 @@ let swatchNum = 7;
 
 const swatches = [
     {
-        mode: 'rgb',
-        r: 0,
-        g: 0,
-        b: 0,
-        alpha: 1
+        value: 0
     },
     {
-        mode: 'rgb',
-        r: 1,
-        g: 1,
-        b: 1,
-        alpha: 1
-    }                        
+        value: 1
+    }
 ]
-
-let neededSwatches = swatchNum - swatches.length;
 
 // add swatches until you reach the max (swatchNum)
 function interpolateValue(num) {
     // black value
-    const minValue = swatches[0].r;
+    const minValue = swatches[0].value;
     // white value
-    const maxValue = swatches[swatches.length-1].r;
+    const maxValue = swatches[swatches.length - 1].value;
     const totalValue = maxValue - minValue;
     // how much to subtract from the value of the next swatch
-    const increment = totalValue/num;
+    const increment = totalValue / num;
 
     let currentValue = totalValue;
 
-    while (swatches.length < num){
+    while (swatches.length < num) {
 
         currentValue = currentValue - increment;
 
         swatches.splice(1, 0, {
-            mode: 'rgb',
-            r: currentValue,
-            g: currentValue,
-            b: currentValue,
-            alpha: 1
+            value: currentValue
         });
 
         console.log(`current value 3: ${currentValue}`);
     };
 };
 
-export default function Palette(){
+function culorifyValues(arr) {
+    // iterate over array
+    for (let i = 0; i < arr.length; i++) {
+        // TODO think i can make this generalize by
+        // if r, g, and b are in the object already, then multiply r, g, and b by value
+        // if not, do the rest of this
+        arr[i].r = arr[i].value;
+        arr[i].g = arr[i].value;
+        arr[i].b = arr[i].value;
+    }
+};
+
+export default function Palette() {
     interpolateValue(swatchNum);
+    culorifyValues(swatches);
     return (
         <div id='Swatches'>
             {swatches.map((swatch, index) => {
                 let color = formatHex8(swatch);
                 return (
-                    <div className='Swatch' key={index} style={{background: color}}></div>
+                    <div className='Swatch' key={index} style={{ background: color }}></div>
                 )
             })}
         </div>
