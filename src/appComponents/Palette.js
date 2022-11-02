a89c124ff79d4dc83865ba3994093c1f530c9032
goblin <factotumfrankie@gmail.com>
2022-11-02 16:01:23 -0700

    switched from culori to built-in hsl

--- src/appComponents/Palette.js
+++ src/appComponents/Palette.js
@@ -1,11 +1,13 @@
 import './Palette.css';
 // import ReactSlider from 'react-slider';
 // import { useState } from 'react';
-import { formatHex8 } from 'culori';
+// import { formatHex8 } from 'culori';
 
 // https://culorijs.org/api/
 
 // BUILD STATIC VERSION
+
+// to pick a color with that value you lock either red, green, or blue, and pick wahtever you want for the other values
 // set up functions that do the other steps from this:
 // https://twitter.com/goblincodes/status/1586878836553453571?s=20&t=vhQoNDyIRnvPif-CzAXPgQ
 
@@ -15,15 +17,36 @@ import { formatHex8 } from 'culori';
 // mandatory seven swatches, but i'd like to be able to let users change this
 let swatchNum = 7;
 
+// must be 0-100 inclusive
 const swatches = [
     {
-        value: .3
+        value: 20
     },
     {
-        value: 1
+        value: 100
     }
 ]
 
+export default function Palette() {
+    // step 1: interpolate between 'black' and 'white'
+    interpolateValue(swatchNum);
+    return (
+        <div id='Swatches'>
+            {swatches.map((swatch, index) => {
+                let color = `hsl(0 0% ${swatch.value}%)`;
+                return (
+                    <div key={index}>
+                        <div className='Swatch' style={{ background: color }}></div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+// HELPER FUNCTIONS
+
+// STEP 1: VALUES
 // add swatches until you reach the max (swatchNum)
 function interpolateValue(num) {
     // 'black' value = value of first object in swatches array
@@ -49,31 +72,6 @@ function interpolateValue(num) {
     };
 };
 
-function culorifyValues(arr) {
-    // iterate over array
-    for (let i = 0; i < arr.length; i++) {
-        // TODO think i can make this generalize by
-        // if r, g, and b are in the object already, then multiply r, g, and b by value
-        // if not, do the rest of this
-        arr[i].r = arr[i].value;
-        arr[i].g = arr[i].value;
-        arr[i].b = arr[i].value;
-    }
-};
-
-export default function Palette() {
-    interpolateValue(swatchNum);
-    culorifyValues(swatches);
-    return (
-        <div id='Swatches'>
-            {swatches.map((swatch, index) => {
-                let color = formatHex8(swatch);
-                return (
-                    <div>
-                        <div className='Swatch' key={index} style={{ background: color }}></div>
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
+// STEP 2: HUES
+// pick 1 swatch
+// either R, G, or B must continue to be the same as VALUE
