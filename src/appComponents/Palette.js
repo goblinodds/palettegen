f9e7a9e70623b01f45ff218fab8eba028f273a9e
goblin <factotumfrankie@gmail.com>
2022-11-08 18:02:30 -0800

    tag to track type of palette, e.g. monochrome

--- src/appComponents/Palette.js
+++ src/appComponents/Palette.js
@@ -5,10 +5,20 @@ import './Palette.css';
 // https://culorijs.org/api/
 
 // BUILD STATIC VERSION
+// monochrome is default
+// analogous - pick a swatch to change the color of
+// complimentary - pick a swatch to change the color of
+// split complimentary - pick 2?? swatches
+// triadic - pick 2?3?? swatches
+// tetradic - pick 3?4 swatches??
+// thinking these last ones should maybe be random
+// https://twitter.com/goblincodes/status/1586878853158342656?s=20&t=CSyVnXYNbcP5RAQEA4i-8A
 
-// to pick a color with that value you lock either red, green, or blue, and pick wahtever you want for the other values
-// set up functions that do the other steps from this:
-// https://twitter.com/goblincodes/status/1586878836553453571?s=20&t=vhQoNDyIRnvPif-CzAXPgQ
+// something that changes value gaps between each!
+// like a checkbox for "consitent value gaps"
+// and if you uncheck it you can change each
+
+// tells you what the current type is-- monochrome, etc.
 
 // LATER VERSIONS
 //// increase/decrease # of swatches: https://twitter.com/goblincodes/status/1586880288495263744?s=20&t=vhQoNDyIRnvPif-CzAXPgQ
@@ -16,6 +26,7 @@ import './Palette.css';
 // mandatory seven swatches, but i'd like to be able to let users change this
 let swatchNum = 7;
 let lockedSwatchIndex = 3;
+let currentScheme = 'monochrome';
 
 // must be 0-100 inclusive
 // TODO let user input values
@@ -33,21 +44,23 @@ export default function Palette() {
     interpolateValue(swatchNum);
     // step 2: pick a hue out of 359
     //TODO let user input hue
-    newHue(200);
+    newHue(50);
     decreaseSaturation(28);
     temperature(-12);
 
     return (
-        <div id='Swatches'>
-            {swatches.map((swatch, index) => {
-                let color = `hsl(${swatch.hue}deg ${swatch.saturation}% ${swatch.value}%)`;
-                console.log(color)
-                return (
-                    <div key={index}>
-                        <div className='Swatch' style={{ background: color }}></div>
-                    </div>
-                )
-            })}
+        <div>
+            <div id='Swatches'>
+                {swatches.map((swatch, index) => {
+                    let color = `hsl(${swatch.hue}deg ${swatch.saturation}% ${swatch.value}%)`;
+                    return (
+                        <div key={index}>
+                            <div className='Swatch' style={{ background: color }}></div>
+                        </div>
+                    )
+                })}
+            </div>
+            <div id='Label'>{currentScheme} palette</div>
         </div>
     )
 }
@@ -88,25 +101,6 @@ function newHue(selectedHue) {
         swatches[i].hue = selectedHue;
         swatches[i].saturation = 100;
     }
-}
-
-// incrementing away from locked swatch
-function spectrum(increment, key) {
-    for (let i = 0; i < swatches.length; i++) {
-        let currentSwatch = swatches[i];
-        // number of swatches from current to locked
-        let swatchesToIncrement;
-        if (i <= lockedSwatchIndex) {
-            swatchesToIncrement = lockedSwatchIndex - i;
-            // SUBTRACT the increment times however many swatches away from locked the current swatch is
-            currentSwatch.key = currentSwatch.key - (increment*swatchesToIncrement);
-        } else if (i > lockedSwatchIndex) {
-            swatchesToIncrement = i - lockedSwatchIndex;
-            // SUBTRACT the increment times however many swatches away from locked the current swatch is
-            currentSwatch.key = currentSwatch.key + (increment*swatchesToIncrement);
-        }
-    }
-    console.log(swatches);
 }
 
 // STEP 3: SATURATION
@@ -154,7 +148,6 @@ function temperature(increment) {
         } else if (currentSwatch.hue < 0) {
             currentSwatch.hue = currentSwatch.hue+360
         }
-        console.log(currentSwatch.hue);
     }
 
 }
